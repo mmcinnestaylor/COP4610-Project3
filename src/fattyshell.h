1150c8442f0b83789b42c0540a6cf428e67de3e7
Marlan McInnes-Taylor <contact@marlanmcinnestaylor.com>
2019-11-17 14:02:28 -0500

    FAT struct

--- src/fattyshell.h
+++ src/fattyshell.h
@@ -52,7 +52,33 @@ typedef struct cmd_t
 typedef struct boot_t
 {
     uint8_t BS_jmpBoot[3];
-    
+    uint8_t BS_OEMName[8];
+    uint8_t BPB_BytsPerSec[2];
+    uint8_t BPB_SecPerClus;
+    uint8_t BPB_RsvdSecCnt[2];
+    uint8_t BPB_NumFATs;
+    uint8_t BPB_RootEntCnt[2];
+    uint8_t BPB_TotSec16[2];
+    uint8_t BPB_Media;
+    uint8_t BPB_FATSz16[2];
+    uint8_t BPB_SecPerTrk[2];
+    uint8_t BPB_NumHeads[2];
+    uint8_t BPB_HiddSec[4];
+    uint8_t BPB_TotSec32[4];
+    uint8_t BS_DrvNum;
+    uint8_t BS_Reserved1;
+    uint8_t BS_BootSig;
+    uint8_t BS_VolID;
+    uint8_t BS_VolLab[11];
+    uint8_t BS_FilSysType[8];
+    uint8_t Signature_word[2];
+    uint8_t BPB_FATSz32[4];
+    uint8_t BPB_ExtFlags[2];
+    uint8_t BPB_FSVer[2];
+    uint8_t BPB_RootClus[4];
+    uint8_t BPB_FSInfo[2];
+    uint8_t BPB_BkBootSec[2];
+    uint8_t BPB_Reserved[12];
 } __attribute__ ((packed)) boot;
 
 typedef struct fat_t 
